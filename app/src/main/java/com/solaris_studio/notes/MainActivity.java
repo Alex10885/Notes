fb40cb598b0d5e0a02f4be7cd751e40020545aea
Alex10885 <aledor10885@gmail.com>
2021-04-25 17:16:16 +0300

    добавлена возможность удаления записей с базы данных

--- app/src/main/java/com/solaris_studio/notes/MainActivity.java
+++ app/src/main/java/com/solaris_studio/notes/MainActivity.java
@@ -20,8 +20,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerViewNotes;
-    public static final ArrayList<Notes> notes = new ArrayList<>();
+    private final ArrayList<Notes> notes = new ArrayList<>();
+    private NotesAdapter adapter;
     private NotesDBHelper dbHelper;
+    private SQLiteDatabase database;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,42 +33,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recyclerViewNotes = findViewById(R.id.recyclerViewNotes);
         dbHelper = new NotesDBHelper(this);
-        SQLiteDatabase database = dbHelper.getWritableDatabase();
-//        if (notes.isEmpty()) {
-//            notes.add(new Notes("Парикмахер", "Сделать прическу", "Понедельник", 2));
-//            notes.add(new Notes("Футбол", "Сыграть с командой во дворе", "Вторник", 3));
-//            notes.add(new Notes("Магазин", "Купить новые джинсы", "Среда", 1));
-//            notes.add(new Notes("Стоматолог", "Вылечить зубы", "Четверг", 2));
-//            notes.add(new Notes("Баскетбол", "Сыграть со школьной командой", "Пятница", 1));
-//        }
-//        for (Notes notes : notes) {
-//            ContentValues contentValues = new ContentValues();
-//            contentValues.put(NotesContract.NotesEntry.COLUMN_TITLE, notes.getTitle());
-//            contentValues.put(NotesContract.NotesEntry.COLUMN_DESCRIPTION, notes.getDescription());
-//            contentValues.put(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK, notes.getDayOfWeek());
-//            contentValues.put(NotesContract.NotesEntry.COLUMN_PRIORITY, notes.getPriority());
-//            database.insert(NotesContract.NotesEntry.TABLE_NAME, null, contentValues);
-//        }
-        ArrayList<Notes> notesFromDB = new ArrayList<>();
-        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null,
-                null, null, null, null);
-         while (cursor.moveToNext()){
-             String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
-             String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
-             String dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
-             int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
-             Notes note = new Notes(title, description, dayOfWeek, priority);
-             notesFromDB.add(note);
-
-         }
-         cursor.close();
-        NotesAdapter adapter = new NotesAdapter(notesFromDB);
+        database = dbHelper.getWritableDatabase();
+        getData();
+        adapter = new NotesAdapter(notes);
         recyclerViewNotes.setLayoutManager(new LinearLayoutManager(this));
         recyclerViewNotes.setAdapter(adapter);
         adapter.setOnNoteClickListener(new NotesAdapter.OnNoteClickListener() {
             @Override
             public void onNoteClick(int position) {
-            Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -88,13 +65,34 @@ public class MainActivity extends AppCompatActivity {
         itemTouchHelper.attachToRecyclerView(recyclerViewNotes);
     }
 
-    private void remove (int position){
-        notes.remove(position);
-
+    private void remove(int position) {
+        int id = notes.get(position).getId();
+        String where = NotesContract.NotesEntry._ID + " = ?";
+        String[] whereArgs = new String[]{Integer.toString(id)};
+        getData();
+        database.delete(NotesContract.NotesEntry.TABLE_NAME, where, whereArgs);
+        getData();
+        adapter.notifyDataSetChanged();
     }
 
     public void onClickAddNotes(View view) {
         Intent intent = new Intent(this, addNodeActivity.class);
         startActivity(intent);
+    }
+
+    private void getData() {
+        notes.clear();
+        Cursor cursor = database.query(NotesContract.NotesEntry.TABLE_NAME, null, null,
+                null, null, null, NotesContract.NotesEntry.COLUMN_PRIORITY);
+        while (cursor.moveToNext()) {
+            int id = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry._ID));
+            String title = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DESCRIPTION));
+            String dayOfWeek = cursor.getString(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_DAY_OF_WEEK));
+            int priority = cursor.getInt(cursor.getColumnIndex(NotesContract.NotesEntry.COLUMN_PRIORITY));
+            Notes note = new Notes(id, title, description, dayOfWeek, priority);
+            notes.add(note);
+        }
+        cursor.close();
     }
 }
--- app/src/main/java/com/solaris_studio/notes/Notes.java
+++ app/src/main/java/com/solaris_studio/notes/Notes.java
@@ -1,16 +1,22 @@
 package com.solaris_studio.notes;
 
 public class Notes {
+    private int id;
     private String title;
     private String description;
     private String dayOfWeek;
     private int priority;
 
-    public Notes(String title, String description, String dayOfWeek, int priority) {
+    public Notes(int id, String title, String description, String dayOfWeek, int priority) {
+        this.id = id;
         this.title = title;
         this.description = description;
         this.dayOfWeek = dayOfWeek;
         this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getTitle() {
